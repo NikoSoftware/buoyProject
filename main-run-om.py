dcf49f2ebe425f87fbb88c0b32fc20da72c06a36
niko <1194142140@qq.com>
2025-06-19 23:42:30 +0800

    ALSA 驱动不支持，移除音频功能

--- main-run-om.py
+++ main-run-om.py
@@ -2,9 +2,6 @@ import cv2
 import numpy as np
 from ais_bench.infer.interface import InferSession
 import time
-import threading
-import simpleaudio as sa
-
 
 # ====================== 配置参数 ======================
 MODEL_PATH = "./runs/train/train6/weights/best.om"
@@ -14,26 +11,6 @@ CONF_THRESH = 0.3  # 置信度阈值
 NMS_THRESH = 0.35  # NMS阈值
 INPUT_SIZE = (640, 640)  # 模型输入尺寸
 SHOW_WINDOW = True  # 控制是否显示实时检测窗口[2,3](@ref)
-
-  # 确保文件存在[9,10](@ref)
-is_playing = False  # 声音播放状态标志
-
-
-def play_alert():
-    """非阻塞播放声音（独立线程）"""
-    global is_playing
-    if not is_playing:
-        # 播放结束后自动更新状态
-        threading.Thread(target=wait_for_sound_end).start()
-
-
-def wait_for_sound_end():
-    global is_playing
-    is_playing = True
-    wave_obj = sa.WaveObject.from_wave_file("./media/seekbar.wav")
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    is_playing = False
 
 
 def preprocess(frame):
@@ -156,10 +133,6 @@ def main():
 
         # 打印检测结果
         print(f"检测到 {len(detections)} 个目标:")
-
-        if len(detections) > 0 :
-            play_alert()
-
         for i, det in enumerate(detections):
             print(f"  目标 {i + 1}: {det['class']} | "
                   f"置信度: {det['confidence']:.4f} | "
@@ -203,6 +176,7 @@ def main():
     cap.release()
     if SHOW_WINDOW:
         cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
