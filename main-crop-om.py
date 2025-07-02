1a4dbc4cdba42cb583eec0b01405912c94a4298c
niko <1194142140@qq.com>
2025-07-03 00:53:52 +0800

    重新训练s模型

--- main-crop-om.py
+++ main-crop-om.py
@@ -9,7 +9,7 @@ import sys
 MODEL_PATH = "./runs/train/train2/weights/best.om"
 VIDEO_PATH = "./datasets/test/30386095338-1-192.mp4"
 CLASS_NAMES = ["buoy"]  # 根据你的buoy.yaml修改类别名称
-CONF_THRESH = 0.3  # 置信度阈值
+CONF_THRESH = 0.5  # 置信度阈值
 NMS_THRESH = 0.65  # NMS阈值
 INPUT_SIZE = (640, 640)  # 模型输入尺寸
 SHOW_WINDOW = True  # 控制是否显示实时检测窗口[2,3](@ref)
